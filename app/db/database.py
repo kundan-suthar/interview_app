c98185bc2b898a6d8b4d73fb1c96044d67718075
kundan suthar <kundanss22@gmail.com>
2026-05-10 16:09:25 +0530

    add prod databse

--- app/db/database.py
+++ app/db/database.py
@@ -5,9 +5,14 @@ from fastapi import Depends
 from app.models.user import Base
 from app.models.user_profile import UserProfile
 from app.models.mock_interview import MockInterview
+from dotenv import load_dotenv
+load_dotenv()
+import os 
 
 # Use asyncpg for PostgreSQL
-DATABASE_URL = f"postgresql+asyncpg://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+# DATABASE_URL = f"postgresql+asyncpg://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_async_engine(DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
